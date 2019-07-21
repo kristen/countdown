2798dad4ffd9cd79ff44b88fdf9faa7832127016
Kristen Sundquist <kssundquist@gmail.com>
2019-07-21 14:53:11 -0700

    Step 7: Add AuthController

---
+++ lib/countdown_web/controllers/auth_controller.exs
@@ -0,0 +1,35 @@
+defmodule CountdownWeb.AuthController do
+  use CountdownWeb, :controller
+
+  alias Countdown.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{token: auth.credentials.token, email: auth.infor.email, provider: "google"}
+    changeset = User.changeset(%User{}, user_params)
+
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: developer_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "error signing in")
+        |> redirect(to: developer_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
+end
